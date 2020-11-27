4b615641152620b380e0acff895957d459dd1d7f
Yuxuan Jiang <jiang700@mc18.cs.purdue.edu>
2020-11-26 22:15:17 -0500

    change train size from 100000 to 200000

--- proj3-skeleton/preprocess.py
+++ proj3-skeleton/preprocess.py
@@ -33,8 +33,8 @@ X_valid, Y_valid = reshape_instances(valid)
 
 # Use a subset of data to save time
 # You can change it in Part(III) to improve your result
-X_train = X_train[:100000]
-Y_train = Y_train[:100000]
+X_train = X_train[:200000]
+Y_train = Y_train[:200000]
 X_test = X_test[:25000]
 Y_test = Y_test[:25000]
 X_valid = X_valid[:25000]
